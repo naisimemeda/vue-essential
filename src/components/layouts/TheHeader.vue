c620ec31357288ca5e84e24f2a96aded830d185a
naisimemeda <2514430140@qq.com>
2019-05-08 23:52:43 +0800

    新增路由 注册界面

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,34 +1,41 @@
 <template>
-  <div class="navbar navbar-default topnav">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" @click="toggleNav">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+<div class="navbar navbar-default topnav">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" @click="toggleNav">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 
-        <a href="/" class="navbar-brand">
-          <span class="title">{{ logo.title }}</span>
-          <img :src="logo.src" :alt="logo.title">
-        </a>
-      </div>
+      <router-link to="/" class="navbar-brand">
+        <span class="title">{{ logo.title }}</span>
+        <img :src="logo.src" :alt="logo.title">
+      </router-link>
+    </div>
 
-      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
-        <ul class="nav navbar-nav">
-          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
-            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
-          </li>
-        </ul>
+    <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+      <ul class="nav navbar-nav">
+        <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+          <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+        </li>
+      </ul>
+      <div class="navbar-right">
+        <TheEntry />
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
+import TheEntry from '@/components/layouts/TheEntry'
 export default {
   name: 'TheHeader',
+  components: {
+    TheEntry
+  },
   data() {
     return {
       logo: {
@@ -55,6 +62,11 @@ export default {
 </script>
 
 <style scoped>
-.title { display: none;}
-.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+.title {
+  display: none;
+}
+
+.navbar-default .navbar-nav>.active>a {
+  background: rgba(0, 0, 0, .03);
+}
 </style>

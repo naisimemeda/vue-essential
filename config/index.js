5e7f4b5c1c8f30ceae8ca14e876d851f0880cd65
naisimemeda <2514430140@qq.com>
2019-05-08 23:02:26 +0800

    git pages 修改

--- config/index.js
+++ config/index.js
@@ -37,13 +37,13 @@ module.exports = {
   },
 
   build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+      // Template for index.html
+      index: path.resolve(__dirname, '../dist/index.html'),
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuejs-essential/dist/',
+      // Paths
+      assetsRoot: path.resolve(__dirname, '../dist'),
+      assetsSubDirectory: 'static',
+      assetsPublicPath: '/vuejs-essential/dist/',
 
     /**
      * Source Maps

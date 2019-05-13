d2ac616f5122b8360b30aab7e9100f23c79850e6
naisimemeda <2514430140@qq.com>
2019-05-13 21:00:47 +0800

    添加vuex 路由导航

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from './App'
 import router from './router'
 import './directives'
 import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // 注入 store
+  store,
   components: { App },
   template: '<App/>'
 })

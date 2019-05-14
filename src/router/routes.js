aa751d4e1e22719a119a9f792267659bc1db7a2b
naisimemeda <2514430140@qq.com>
2019-05-14 21:17:01 +0800

    头像上传 密码修改

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,5 @@
-export default [{
+export default [
+  {
     path: '/auth/register',
     name: 'Register',
     component: () => import('@/views/auth/Register')
@@ -17,18 +18,29 @@ export default [{
     name: 'Login',
     component: () => import('@/views/auth/Login')
   },
-  // EditUsers
   {
     path: '/users/1/edit',
     component: () => import('@/views/users/Edit.vue'),
-    children: [{
-      path: '',
-      name: 'EditProfile',
-      component: () => import('@/views/users/Profile.vue'),
-      // auth 为 true，标识当前路由需要登录才能访问
-      meta: {
-        auth: true
+    children: [
+      {
+        path: '',
+        name: 'EditProfile',
+        component: () => import('@/views/users/Profile.vue'),
+        meta: { auth: true }
+      },
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar.vue'),
+        meta: { auth: true }
+      },
+      // EditPassword
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password.vue'),
+        meta: { auth: true }
       }
-    }]
+    ]
   },
 ]

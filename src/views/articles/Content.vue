a7eb77402fe090f726a3e5cbedcb7ebba8dae97d
naisimemeda <2514430140@qq.com>
2019-05-15 17:55:46 +0800

    添加个人专栏 点赞功能

--- src/views/articles/Content.vue
+++ src/views/articles/Content.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="blog-container" style="margin-top:20px">
-    <div class="blog-pages">
-      <div class="col-md-9 left-col pull-right">
-        <div class="panel article-body content-body">
-          <h1 class="text-center">{{ title }}</h1>
-          <div class="article-meta text-center">
-            <i class="fa fa-clock-o"></i>
-            <abbr>{{ date | moment('from') }}</abbr>
-          </div>
-          <div class="entry-content">
-            <div class="content-body entry-content panel-body ">
-              <div class="markdown-body" v-html="content"></div>
-              <div v-if="auth && uid === 1" class="panel-footer operate">
-                <div class="actions">
-                  <a @click="deleteArticle" class="admin" href="javascript:;"><i class="fa fa-trash-o"></i></a>
-                  <a @click="editArticle" class="admin" href="javascript:;"><i class="fa fa-pencil-square-o"></i></a>
-                </div>
-              </div>
+  <div class="col-md-9 left-col pull-right">
+    <div class="panel article-body content-body">
+      <h1 class="text-center">{{ title }}</h1>
+      <div class="article-meta text-center">
+        <i class="fa fa-clock-o"></i>
+        <abbr>{{ date | moment('from') }}</abbr>
+      </div>
+      <div class="entry-content">
+        <div class="content-body entry-content panel-body ">
+          <div class="markdown-body" v-html="content"></div>
+
+          <div v-if="auth && uid === 1" class="panel-footer operate">
+            <div class="actions">
+              <a @click="deleteArticle" class="admin" href="javascript:;"><i class="fa fa-trash-o"></i></a>
+              <a @click="editArticle" class="admin" href="javascript:;"><i class="fa fa-pencil-square-o"></i></a>
             </div>
+          </div>
+        </div>
+      </div>
+      <div class="votes-container panel panel-default padding-md">
+        <div class="panel-body vote-box text-center">
+          <div class="btn-group">
+            <a @click="like" href="javascript:;" class="vote btn btn-primary popover-with-html" :class="likeClass">
+              <i class="fa fa-thumbs-up"></i> {{ likeClass ? '已赞' : '点赞' }}
+            </a>
+          </div>
+          <div class="voted-users">
+            <div class="user-lists">
+        <span v-for="likeUser in likeUsers">
+          <!-- 点赞用户是当前用户时，加上类 animated 和 swing 以显示一个特别的动画  -->
+          <img class="animated swing">
+          <img :src="user && user.avatar" class="img-thumbnail avatar avatar-middle " :class="{ 'animated swing' : likeUser.uid === 1 }">
+        </span>
+            </div>
+            <div v-if="!likeUsers.length" class="vote-hint">成为第一个点赞的人吧 ? </div>
           </div>
         </div>
       </div>
@@ -38,7 +54,9 @@
         title: '', // 文章标题
         content: '', // 文章内容
         date: '', // 文章创建时间
-        uid: 1 // 用户 ID
+        uid: 1, // 用户 ID
+        likeUsers: [], // 点赞用户列表
+        likeClass: '', // 点赞样式
       }
     },
     computed: {
@@ -52,12 +70,16 @@
       const article = this.$store.getters.getArticleById(articleId)
 
       if (article) {
-        let { uid, title, content, date } = article
+        let { uid, title, content, date, likeUsers } = article
 
         this.uid = uid
         this.title = title
         this.content = SimpleMDE.prototype.markdown(emoji.emojify(content, name => name))
         this.date = date
+        // 更新实例的 likeUsers
+        this.likeUsers = likeUsers || []
+        // 更新 likeClass，点赞用户列表包含当前用户时，赋值为 active，表示已赞
+        this.likeClass = this.likeUsers.some(likeUser => likeUser.uid === 1) ? 'active' : ''
 
         this.$nextTick(() => {
           this.$el.querySelectorAll('pre code').forEach((el) => {
@@ -69,6 +91,40 @@
       this.articleId = articleId
     },
     methods: {
+      like(e) {
+        // 未登录时，提示登录
+        if (!this.auth) {
+          this.$swal({
+            text: '需要登录以后才能执行此操作。',
+            confirmButtonText: '前往登录'
+          }).then((res) => {
+            if (res.value) {
+              this.$router.push('/auth/login')
+            }
+          })
+        } else {
+          const target = e.currentTarget
+          // 点赞按钮是否含有 active 类，我们用它来判断是否已赞
+          const active = target.classList.contains('active')
+          const articleId = this.articleId
+
+          if (active) {
+            // 清除已赞样式
+            this.likeClass = ''
+            // 分发 like 事件取消赞，更新实例的 likeUsers 为返回的值
+            this.$store.dispatch('like', { articleId }).then((likeUsers) => {
+              this.likeUsers = likeUsers
+            })
+          } else {
+            // 添加已赞样式
+            this.likeClass = 'active animated rubberBand'
+            // 分发 like 事件，传入 isAdd 参数点赞，更新实例的 likeUsers 为返回的值
+            this.$store.dispatch('like', { articleId, isAdd: true }).then((likeUsers) => {
+              this.likeUsers = likeUsers
+            })
+          }
+        }
+      },
       editArticle() {
         this.$router.push({ name: 'Edit', params: { articleId: this.articleId } })
       },

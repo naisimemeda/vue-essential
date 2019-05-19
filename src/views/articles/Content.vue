a13f57c37d6a69f567f5ce70f801109674a2af4f
naisimemeda <2514430140@qq.com>
2019-05-19 14:32:05 +0800

    添加删除 修改评论功能

--- src/views/articles/Content.vue
+++ src/views/articles/Content.vue
@@ -1,123 +1,137 @@
 <template>
-  <div class="col-md-9 left-col pull-right">
-    <div class="panel article-body content-body">
-      <h1 class="text-center">{{ title }}</h1>
-      <div class="article-meta text-center">
-        <i class="fa fa-clock-o"></i>
-        <abbr>{{ date | moment('from', { startOf: 'minute' }) }}</abbr>
-      </div>
-      <div class="entry-content">
-        <div class="content-body entry-content panel-body ">
-          <div class="markdown-body" v-html="content"></div>
+<div class="col-md-9 left-col pull-right">
+  <div class="panel article-body content-body">
+    <h1 class="text-center">{{ title }}</h1>
+    <div class="article-meta text-center">
+      <i class="fa fa-clock-o"></i>
+      <abbr>{{ date | moment('from', { startOf: 'minute' }) }}</abbr>
+    </div>
+    <div class="entry-content">
+      <div class="content-body entry-content panel-body ">
+        <div class="markdown-body" v-html="content"></div>
 
-          <div v-if="auth && uid === 1" class="panel-footer operate">
-            <div class="actions">
-              <a @click="deleteArticle" class="admin" href="javascript:;"><i class="fa fa-trash-o"></i></a>
-              <a @click="editArticle" class="admin" href="javascript:;"><i class="fa fa-pencil-square-o"></i></a>
-            </div>
+        <div v-if="auth && uid === 1" class="panel-footer operate">
+          <div class="actions">
+            <a @click="deleteArticle" class="admin" href="javascript:;"><i class="fa fa-trash-o"></i></a>
+            <a @click="editArticle" class="admin" href="javascript:;"><i class="fa fa-pencil-square-o"></i></a>
           </div>
         </div>
       </div>
-    </div>
-
-    <!-- 点赞 -->
-    <div class="votes-container panel panel-default padding-md">
-      <div class="panel-body vote-box text-center">
-				<div class="btn-group">
-				  <a @click="like" href="javascript:;" class="vote btn btn-primary popover-with-html" :class="likeClass">
-				    <i class="fa fa-thumbs-up"></i> {{ likeClass ? '已赞' : '点赞' }}
-				  </a>
-				  <div class="or"></div>
-				  <button @click="showQrcode = true" class="btn btn-success"><i class="fa fa-heart"></i> 打赏</button>
-				</div>
-        <div class="voted-users">
-          <div class="user-lists">
-            <span v-for="likeUser in likeUsers">
-              <!-- 点赞用户是当前用户时，加上类 animated 和 swing 以显示一个特别的动画  -->
-              <img :src="user && user.avatar" class="img-thumbnail avatar avatar-middle" :class="{ 'animated swing' : likeUser.uid === 1 }">
-            </span>
-          </div>
-          <div v-if="!likeUsers.length" class="vote-hint">成为第一个点赞的人吧 😄</div>
-        </div>
-      </div>
-    </div>
-
-    <!-- 打赏弹窗 -->
-    <Modal :show.sync="showQrcode" class="text-center">
-      <div v-if="user" slot="title">
-        <img :src="user.avatar" class="img-thumbnail avatar" width="48">
-      </div>
-      <div>
-        <p class="text-md">如果你想学习更多前端的知识，VuejsCaff.com 是个不错的开始</p>
-        <div class="payment-qrcode inline-block">
-          <h5>扫一扫打开 VuejsCaff.com</h5>
-          <p><qrcode-vue value="https://vuejscaff.com/" :size="160"></qrcode-vue></p>
-        </div>
-      </div>
-      <div slot="footer">
-        <div class="text-center">祝你学习愉快 :)</div>
-      </div>
-    </Modal>
-
-    <!-- 评论列表 -->
-    <div class="replies panel panel-default list-panel replies-index">
-      <div class="panel-heading">
-        <div class="total">
-          回复数量: <b>{{ comments.length }}</b>
-        </div>
-      </div>
-      <div class="panel-body">
-        <transition-group id="reply-list" name="fade" tag="ul" class="list-group row">
-          <li v-for="(comment, index) in comments" :key="comment.commentId" class="list-group-item media">
-            <div class="avatar avatar-container pull-left">
-              <router-link :to="`/${comment.uname}`">
-                <img :src="comment.uavatar" class="media-object img-thumbnail avatar avatar-middle">
-              </router-link>
-            </div>
-            <div class="infos">
-              <div class="media-heading">
-                <router-link :to="`/${comment.uname}`" class="remove-padding-left author rm-link-color">
-                  {{ comment.uname }}
-                </router-link>
-                <div class="meta">
-                  <a :id="`reply${index + 1}`" :href="`#reply${index + 1}`" class="anchor">#{{ index + 1 }}</a>
-                  <span> ⋅ </span>
-                  <abbr class="timeago">
-                    {{ comment.date | moment('from', { startOf: 'second' }) }}
-                  </abbr>
-                </div>
-              </div>
-
-              <div class="preview media-body markdown-reply markdown-body" v-html="comment.content"></div>
-            </div>
-          </li>
-        </transition-group>
-        <div v-show="!comments.length" class="empty-block">
-          暂无评论~~
-        </div>
-      </div>
-    </div>
-
-    <!-- 评论框 -->
-    <div id="reply-box" class="reply-box form box-block">
-      <div class="form-group comment-editor">
-        <textarea v-if="auth" id="editor"></textarea>
-        <textarea v-else disabled class="form-control" placeholder="需要登录后才能发表评论." style="height:172px"></textarea>
-      </div>
-      <div class="form-group reply-post-submit">
-        <button id="reply-btn" :disabled="!auth" @click="comment" class="btn btn-primary">回复</button>
-        <span class="help-inline">Ctrl+Enter</span>
-      </div>
-      <div v-show="commentHtml" id="preview-box" class="box preview markdown-body" v-html="commentHtml"></div>
     </div>
   </div>
+
+  <!-- 点赞 -->
+  <div class="votes-container panel panel-default padding-md">
+    <div class="panel-body vote-box text-center">
+      <div class="btn-group">
+        <a @click="like" href="javascript:;" class="vote btn btn-primary popover-with-html" :class="likeClass">
+          <i class="fa fa-thumbs-up"></i> {{ likeClass ? '已赞' : '点赞' }}
+        </a>
+        <div class="or"></div>
+        <button @click="showQrcode = true" class="btn btn-success"><i class="fa fa-heart"></i> 打赏</button>
+      </div>
+      <div class="voted-users">
+        <div class="user-lists">
+          <span v-for="likeUser in likeUsers">
+            <!-- 点赞用户是当前用户时，加上类 animated 和 swing 以显示一个特别的动画  -->
+            <img :src="user && user.avatar" class="img-thumbnail avatar avatar-middle" :class="{ 'animated swing' : likeUser.uid === 1 }">
+          </span>
+        </div>
+        <div v-if="!likeUsers.length" class="vote-hint">成为第一个点赞的人吧 😄</div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 打赏弹窗 -->
+  <Modal :show.sync="showQrcode" class="text-center">
+    <div v-if="user" slot="title">
+      <img :src="user.avatar" class="img-thumbnail avatar" width="48">
+    </div>
+    <div>
+      <p class="text-md">如果你想学习更多前端的知识，VuejsCaff.com 是个不错的开始</p>
+      <div class="payment-qrcode inline-block">
+        <h5>扫一扫打开 VuejsCaff.com</h5>
+        <p>
+          <qrcode-vue value="https://vuejscaff.com/" :size="160"></qrcode-vue>
+        </p>
+      </div>
+    </div>
+    <div slot="footer">
+      <div class="text-center">祝你学习愉快 :)</div>
+    </div>
+  </Modal>
+
+  <!-- 评论列表 -->
+  <div class="replies panel panel-default list-panel replies-index">
+    <div class="panel-heading">
+      <div class="total">
+        回复数量: <b>{{ comments.length }}</b>
+      </div>
+    </div>
+    <div class="panel-body">
+      <transition-group id="reply-list" name="fade" tag="ul" class="list-group row">
+        <li v-for="(comment, index) in comments" :key="comment.commentId" class="list-group-item media">
+          <div class="avatar avatar-container pull-left">
+            <router-link :to="`/${comment.uname}`">
+              <img :src="comment.uavatar" class="media-object img-thumbnail avatar avatar-middle">
+            </router-link>
+          </div>
+          <div class="infos">
+            <div class="media-heading">
+              <router-link :to="`/${comment.uname}`" class="remove-padding-left author rm-link-color">
+                {{ comment.uname }}
+              </router-link>
+              <span v-if="auth" class="operate pull-right">
+                <span v-if="comment.uid === 1">
+                  <a href="javascript:;" @click="editComment(comment.commentId, index)"><i class="fa fa-edit"></i></a>
+                  <span> ⋅ </span>
+                  <a href="javascript:;" @click="deleteComment(comment.commentId)"><i class="fa fa-trash-o"></i></a>
+                </span>
+              </span>
+              <div class="meta">
+                <a :id="`reply${index + 1}`" :href="`#reply${index + 1}`" class="anchor">#{{ index + 1 }}</a>
+                <span> ⋅ </span>
+                <abbr class="timeago">
+                  {{ comment.date | moment('from', { startOf: 'second' }) }}
+                </abbr>
+              </div>
+            </div>
+
+            <div class="preview media-body markdown-reply markdown-body" v-html="comment.content"></div>
+          </div>
+        </li>
+      </transition-group>
+      <div v-show="!comments.length" class="empty-block">
+        暂无评论~~
+      </div>
+    </div>
+  </div>
+
+  <!-- 评论框 -->
+  <div id="reply-box" class="reply-box form box-block">
+    <div class="form-group comment-editor">
+      <textarea v-if="auth" id="editor"></textarea>
+      <textarea v-else disabled class="form-control" placeholder="需要登录后才能发表评论." style="height:172px"></textarea>
+    </div>
+    <div class="form-group reply-post-submit">
+      <button id="reply-btn" :disabled="!auth" @click="comment" class="btn btn-primary">
+        {{ commentId ? '保存编辑' : '回复' }}
+      </button>
+      <span v-show="commentId" class="help-inline btn-cancel" style="cursor:pointer" @click="cancelEditComment">取消编辑</span>
+      <span v-show="!commentId" class="help-inline">Ctrl+Enter</span>
+    </div>
+    <div v-show="commentHtml" id="preview-box" class="box preview markdown-body" v-html="commentHtml"></div>
+  </div>
+</div>
 </template>
 
 <script>
 import SimpleMDE from 'simplemde'
 import hljs from 'highlight.js'
 import emoji from 'node-emoji'
-import { mapState } from 'vuex'
+import {
+  mapState
+} from 'vuex'
 // 引入 qrcode.vue 的默认值
 import QrcodeVue from 'qrcode.vue'
 
@@ -138,6 +152,7 @@ export default {
       showQrcode: false, // 是否显示打赏弹窗
       commentHtml: '', // 评论 HTML
       comments: [], // 评论列表
+      commentId: undefined, // 评论 ID
     }
   },
   computed: {
@@ -152,7 +167,14 @@ export default {
 
     if (article) {
       // 获取文章的 comments
-      let { uid, title, content, date, likeUsers, comments } = article
+      let {
+        uid,
+        title,
+        content,
+        date,
+        likeUsers,
+        comments
+      } = article
 
       this.uid = uid
       this.title = title
@@ -202,9 +224,11 @@ export default {
 
       // 按键松开监听
       simplemde.codemirror.on('keyup', (codemirror, event) => {
-        // 使用 Ctrl+Enter 时提交评论
         if (event.ctrlKey && event.keyCode === 13) {
           this.comment()
+        } else if (this.commentId && event.keyCode === 27) { // 存在 commentId，且按下 Esc 键时
+          // 取消编辑评论
+          this.cancelEditComment()
         }
       })
 
@@ -214,7 +238,12 @@ export default {
   },
   methods: {
     editArticle() {
-      this.$router.push({ name: 'Edit', params: { articleId: this.articleId } })
+      this.$router.push({
+        name: 'Edit',
+        params: {
+          articleId: this.articleId
+        }
+      })
     },
     deleteArticle() {
       this.$swal({
@@ -222,7 +251,9 @@ export default {
         confirmButtonText: '删除'
       }).then((res) => {
         if (res.value) {
-          this.$store.dispatch('post', { articleId: this.articleId })
+          this.$store.dispatch('post', {
+            articleId: this.articleId
+          })
         }
       })
     },
@@ -247,14 +278,19 @@ export default {
           // 清除已赞样式
           this.likeClass = ''
           // 分发 like 事件取消赞，更新实例的 likeUsers 为返回的值
-          this.$store.dispatch('like', { articleId }).then((likeUsers) => {
+          this.$store.dispatch('like', {
+            articleId
+          }).then((likeUsers) => {
             this.likeUsers = likeUsers
           })
         } else {
           // 添加已赞样式
           this.likeClass = 'active animated rubberBand'
           // 分发 like 事件，传入 isAdd 参数点赞，更新实例的 likeUsers 为返回的值
-          this.$store.dispatch('like', { articleId, isAdd: true }).then((likeUsers) => {
+          this.$store.dispatch('like', {
+            articleId,
+            isAdd: true
+          }).then((likeUsers) => {
             this.likeUsers = likeUsers
           })
         }
@@ -263,24 +299,33 @@ export default {
     comment() {
       if (this.commentMarkdown && this.commentMarkdown.trim() !== '') {
         this.$store.dispatch('comment', {
-          comment: { content: this.commentMarkdown },
-          articleId: this.articleId
-        }).then(this.renderComments) // 在 .then 的回调里，调用 this.renderComments 渲染评论
+          comment: {
+            content: this.commentMarkdown
+          },
+          articleId: this.articleId,
+          // 传入 commentId
+          commentId: this.commentId
+        }).then(this.renderComments)
 
-        this.simplemde.value('')
-        document.querySelector('#reply-btn').focus()
+        if (this.commentId) { // 有 commentId 时，取消编辑评论
+          this.cancelEditComment()
+        } else { // 没有 commentId 时，写入原来的逻辑
+          this.simplemde.value('')
+          document.querySelector('#reply-btn').focus()
 
-        // 将最后的评论滚动到页面的顶部
-        this.$nextTick(() => {
-          const lastComment = document.querySelector('#reply-list li:last-child')
-          if (lastComment) lastComment.scrollIntoView(true)
-        })
+          this.$nextTick(() => {
+            const lastComment = document.querySelector('#reply-list li:last-child')
+            if (lastComment) lastComment.scrollIntoView(true)
+          })
+        }
       }
     },
     renderComments(comments) {
       if (Array.isArray(comments)) {
         // 深拷贝 comments 以不影响其原值
-        const newComments = comments.map(comment => ({ ...comment }))
+        const newComments = comments.map(comment => ({
+          ...comment
+        }))
         const user = this.user || {}
 
         for (let comment of newComments) {
@@ -296,11 +341,76 @@ export default {
         this.commentsMarkdown = comments
       }
     },
+    editComment(commentId, commentIndex) {
+      // 编辑器
+      const simplemde = this.simplemde
+      // 编辑器
+      const codemirror = simplemde.codemirror
+      // Markdown 格式的所有评论
+      const comments = this.commentsMarkdown
+
+      for (const comment of comments) {
+        // 找到与 commentId 对应的评论时
+        if (parseInt(comment.commentId) === parseInt(commentId)) {
+          // 设置编辑器的内容
+          simplemde.value(comment.content)
+          // 使编辑器获得焦点
+          codemirror.focus()
+          // 将光标移到内容的后面
+          codemirror.setCursor(codemirror.lineCount(), 0)
+          // 评论索引 + 1，用来指示页面滚动的位置
+          this.commentIndex = commentIndex + 1
+          // 更新 commentId
+          this.commentId = commentId
+          break
+        }
+      }
+    },
+    // 取消编辑评论
+    cancelEditComment() {
+      // 清除 commentId
+      this.commentId = undefined
+      // 清空编辑器
+      this.simplemde.value('')
+
+      // 下次 DOM 更新后，将评论滚动回视图的顶部
+      this.$nextTick(() => {
+        if (this.commentIndex === undefined) return
+        const currentComment = document.querySelector(`#reply-list li:nth-child(${this.commentIndex})`)
+
+        if (currentComment) {
+          currentComment.scrollIntoView(true)
+          currentComment.querySelector('.operate a').focus()
+        }
+      })
+    },
+    deleteComment(commentId) {
+      this.$swal({
+        text: '你确定要删除此评论吗?',
+        confirmButtonText: '删除'
+      }).then((res) => {
+        if (res.value) {
+          // 此时不用传入 comment
+          this.$store.dispatch('comment', {
+            commentId,
+            articleId: this.articleId
+          }).then(this.renderComments)
+          this.cancelEditComment()
+        }
+      })
+    },
   }
 }
 </script>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active { transition: opacity .5s;}
-.fade-enter, .fade-leave-to { opacity: 0;}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
